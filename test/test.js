20d749c5a75504e7028bab8431414042371e85e4
Vincent Oh <vincent.form@gmail.com>
2016-06-23 15:24:35 +0800

    Add deploy Script

--- test/test.js
+++ test/test.js
@@ -3,6 +3,7 @@ var app = require('../app.js');
 
 describe('GET /', function(){
     it('respond with hello world',function(done){
+        console.log("Start Testing");
         request(app).get('/').expect('hello world',done);
     })
 })
